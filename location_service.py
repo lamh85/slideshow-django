ca2a1c76f31fa90b548de71159026a1edc5aad88
lamh85 <lamh85@gmail.com>
2024-01-17 21:45:47 -0800

    Make API call to get location name.

--- location_service.py
+++ location_service.py
@@ -1,4 +1,5 @@
 from dotenv import dotenv_values
+import requests
 
 config = dotenv_values(".env")
 open_weather_api_key = config["OPEN_WEATHER_API_KEY"]
@@ -6,6 +7,8 @@ open_weather_api_key = config["OPEN_WEATHER_API_KEY"]
 # TODO: use these arguments
 # longitude_degrees, longitude_minutes, longitude_direction, latitude_degrees, latitude_minutes, latitude_direction
 def get_location_name():
-  print(open_weather_api_key)
+  # TODO: replace these query params with function's arguments
+  response = requests.get(f'http://api.openweathermap.org/geo/1.0/reverse?lat=49.2827&lon=-123.1207&appid={open_weather_api_key}')
+  print(response.text)
 
 print(get_location_name())
