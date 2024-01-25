659b103677ddc101a17824926b84d5057795a921
lamh85 <lamh85@gmail.com>
2024-01-24 16:12:08 -0800

    experiment with logging

--- slideshow/views.py
+++ slideshow/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+import logging
 
 def index(request):
+    # print(request)
+    # logging.info(request)
+    logging.info("request")
     return HttpResponse("Hello, world.")
+    # return HttpResponse(request)
