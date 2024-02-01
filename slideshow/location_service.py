ba72e311f5e1e318e599bb431714aaa775dc5f86
lamh85 <lamh85@gmail.com>
2024-01-31 23:19:18 -0800

    update comment

--- slideshow/location_service.py
+++ slideshow/location_service.py
@@ -6,7 +6,7 @@ open_weather_api_key = config["OPEN_WEATHER_API_KEY"]
 
 # TODO: use these arguments
 # longitude_degrees, longitude_minutes, longitude_direction, latitude_degrees, latitude_minutes, latitude_direction
-# Vancouver, BC: 49.2827 latitude, -123.1207 longitude
+# Vancouver, BC: 49.277933762813475 latitude, -123.1093055007045 longitude
 def get_location_name(params):
     latitude_degrees = params["latitude_degrees"]
     longitude_degrees = params["longitude_degrees"]
