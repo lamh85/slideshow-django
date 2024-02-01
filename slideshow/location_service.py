61e64205e869532a966190bc5df503287bc0c819
lamh85 <lamh85@gmail.com>
2024-01-31 22:37:25 -0800

    Add params to API: latitude_degrees, longitude_degrees

--- slideshow/location_service.py
+++ slideshow/location_service.py
@@ -6,7 +6,10 @@ open_weather_api_key = config["OPEN_WEATHER_API_KEY"]
 
 # TODO: use these arguments
 # longitude_degrees, longitude_minutes, longitude_direction, latitude_degrees, latitude_minutes, latitude_direction
-def get_location_name():
-  # TODO: replace these query params with function's arguments
-  response = requests.get(f'http://api.openweathermap.org/geo/1.0/reverse?lat=49.2827&lon=-123.1207&appid={open_weather_api_key}')
-  return response.text
+# Vancouver, BC: 49.2827 latitude, -123.1207 longitude
+def get_location_name(params):
+    latitude_degrees = params["latitude_degrees"]
+    longitude_degrees = params["longitude_degrees"]
+
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/reverse?lat={latitude_degrees}&lon={longitude_degrees}&appid={open_weather_api_key}')
+    return response.text
--- slideshow/views.py
+++ slideshow/views.py
@@ -1,15 +1,11 @@
 from django.http import HttpResponse
-import logging
 import json
 from . import location_service
 
 def index(request):
     request_body = request.body
-    body_dict = json.loads(request_body.decode("utf-8"))
+    params = json.loads(request_body.decode("utf-8"))
 
-    logger = logging.getLogger("general")
-    logger.info(body_dict["property"])
-
-    location_result = location_service.get_location_name()
+    location_result = location_service.get_location_name(params)
 
     return HttpResponse(location_result)
